de8435a6934d843877dc3635e65ddbd97f684599
Vladimir Syromyatnikov <v.syromyatnikov@netris.ru>
2025-01-07 17:56:17 +0300

    Установлен vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
   server: {
     open: true,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      overlay: false,
+      eslint: {
+        lintCommand: 'eslint .',
+        useFlatConfig: true,
+      },
+      typescript: {
+        tsconfigPath: './tsconfig.app.json',
+      },
+    }),
+  ],
 });
